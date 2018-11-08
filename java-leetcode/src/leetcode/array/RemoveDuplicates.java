e3d3770d60a7214d19d8cd195e85a00ccf854a46
jianweizhao <zhaojw_420@163.com>
2018-11-08 15:23:55 +0800

    原地并且不使用多余的数组空间以及时间/空间复杂度给数据去重

--- java-leetcode/src/leetcode/array/RemoveDuplicates.java
+++ java-leetcode/src/leetcode/array/RemoveDuplicates.java
@@ -8,4 +8,35 @@ package leetcode.array;
  * @date 2018/10/31 21:38
  */
 public class RemoveDuplicates {
+
+    public static void main(String[] args){
+        RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+       // int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums = {1,1,2};
+        removeDuplicates.removeDuplicates(nums);
+    }
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        int length = nums.length;
+        int temp = nums[0];
+        int j = 1;
+        for (int i=1;i<nums.length;i++){
+            if (nums[i] != temp){
+                temp = nums[i];
+                nums[j] = temp;
+                j++;
+            }else {
+                length--;
+
+            }
+        }
+
+        for (int i=0;i<length;i++){
+            System.err.print(nums[i]+",");
+        }
+        return length;
+    }
 }
---
+++ java-leetcode/src/leetcode/array/RemoveElement.java
@@ -0,0 +1,11 @@
+package leetcode.array;
+
+/**
+ * <p></p>
+ *
+ * @author jwzhao
+ * @version 1.0
+ * @date 2018/11/8 15:23
+ */
+public class RemoveElement {
+}
